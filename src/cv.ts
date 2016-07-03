63a885d22180b942fd06ea692f15daa60eec15a2
perjo927 <perjo927@student.liu.se>
2016-07-03 15:12:45 +0200

    Update recommendations and contact

--- src/cv.ts
+++ src/cv.ts
@@ -286,26 +286,61 @@ export class Cv {
         }
     ];
 
-    contact: any = [
-        {
+    contact: any = {
             mail: "djpjgj [at] gmail.com",
             phone: "SE 76 180 55 94",
             web: "http://programmerper.com"
-        }
-    ];
+    };
 
     presence: any = [
         {
-            title: "",
-            handle: "",
-            image: ""
-        }
+            title: "GitHub",
+            handle: "perjo927",
+            image: "github_logo.gif"
+        },
+        {
+            title: "LinkedIn",
+            handle: "djpjgj",
+            image: "linkedin.png"
+        },
+        {
+            title: "WordPress",
+            handle: "programmerper",
+            image: "wordpress.png"
+        },
+        {
+            title: "Twitter",
+            handle: "programmerper",
+            image: "twitter.png"
+        },
+        {
+            title: "SoundCloud",
+            handle: "djpjgj",
+            image: "soundcloud-logo.png"
+        },
+        {
+            title: "Per's Web",
+            handle: "programmerper.com",
+            image: "p-logo.png"
+        },
     ];
 
     recommendations: any = [
         {
-            title: "",
-            text: ""
+            title: "Betsson: Bjorg E",
+            text: "Always on point, trustworthy and a talented coder, a good team player and easy to work with ... Per rocks! "
+        },
+        {
+            title: "Ericsson: Svante S",
+            text: "With his constructive way, Per has helped us to streamline the way we carry out follow-up work ... we give him excellent recommendations"
+        },
+        {
+            title: "Linkoping: Hannah B",
+            text: "Per is one of the most driven, hard-working and sharp person I have worked with."
+        },
+        {
+            title: "CoderDojo: Marcus N",
+            text: " I love Per's drive! ... He has a great approach to problem solving, finds creative solutions ... and is positive and fun to be with. "
         }
     ];
 }
